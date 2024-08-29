4f99b089f290e6401557aa8ebc128ee719756c03
Tamilarasan-112 <Tamilarasan.Ramalingam@trumpf.com>
2024-08-29 18:07:02 +0530

    in this commit the code is modified, Removed unwanted lines,variables and fixed the warning

--- cTraining/cTraining/converter.c
+++ cTraining/cTraining/converter.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 #include<math.h>
+#include<stdbool.h>
 long int i;
 long int h;
 long int arr[128];
 int rev[128];
 long long int n;
-void Hex (long int  n, int* ptr);
+void Hex (long long int  n, int* ptr);
 
 /// <summary>This function get the decimal value from user and display the Binary and Hexadecimal value for the given decimal value.</summary>
-void BinaryHex (long long int n);
+void BinaryAndHexConverter (long long int n);
 
 int main () {
 	//case:pass the decimal value
@@ -17,8 +18,7 @@ int main () {
 	printf ("Please don't enter more then 12 digits number!");
 	long long int n;
 	char c;
-	int i = 1;
-	int a = 0;
+	int i = 1,a = 0;
 	int* p = &a;
 	do {
 		printf ("\nCase%d:\n", i);
@@ -30,23 +30,23 @@ int main () {
 				printf ("Hex:%d", 0);
 			} else {
 				printf ("output:");
-				BinaryHex (n);
+				BinaryAndHexConverter (n);
 			}
 		} else printf ("Invalid!");
 		i++;
-	} while ((*p) == 1);
+	} while ((c=getchar())!='q');
 }
 
-void BinaryHex (long long int n) {
+void BinaryAndHexConverter (long long int n) {
 	printf ("\n");
 	i = 0;
-	long long int temp;
-	temp = n;
-	long long int m = 0;
+	long long int temp= n,m = 0;
+	//if the given integer is signed value 
 	if (n < 0) {
-		for (n; n < 0; n++)m++;
+		for (n; n < 0; n++)m++; //this for loop convert negative value to postive value
 		n = m;
 		printf ("Binary:");
+		//converted to binary
 		while (n > 0) {
 			arr[i] = n % 2;
 			n = round (n / 2);
@@ -56,15 +56,17 @@ void BinaryHex (long long int n) {
 			if (arr[a] == 0) rev[b] = 1;
 			else rev[b] = 0;
 		}
+		//if the digit is 8 or less then 8
 		if (i <=7 && i >= 0) {
-			int j = 0;
-			int z;
+			int j = 0,z;
+			//span and invert(1's compliment)
 			for (j; j <= 7 - i; j++) arr[j] = 1;
 			for (int x = 0; x < i; x++) {
 				arr[j++] = rev[x];
 				h = j-1 ;
 				z = h;
 			}
+			//add 1 at last digit(2's compliment)
 			if (z == 7) {
 				if ((arr[z] == 1)) {
 					while (((arr[z] += 1) == 2)) {
@@ -73,9 +75,9 @@ void BinaryHex (long long int n) {
 					}
 				} else arr[h] = 1;
 			}
+		//if the digit is 16 or less then 16
 		} else if (i <= 15 && i >= 8) {
-		   int j = 0;
-			int z ;
+		   int j = 0,z ;
 			for (j; j < 16 - i; j++) arr[j] = 1;
 			for (int x = 0; x < i; x++) {
 				arr[j++] = rev[x];
@@ -90,9 +92,9 @@ void BinaryHex (long long int n) {
 					}
 				} else arr[h] = 1;
 			}
+		//if the digit is 16 or less then 16
 		} else if (i <= 31 && i >= 16) {
-			int j = 0;
-			int z;
+			int j = 0,z;
 			for (j; j < 32 - i; j++) arr[j] = 1;
 			for (int x = 0; x < i; x++) {
 				arr[j++] = rev[x];
@@ -108,9 +110,9 @@ void BinaryHex (long long int n) {
 				} else arr[h] = 1;
 			}
 		}
+		//if the digit is 64 or less then 64
 		else {
-			int j = 0;
-			int z;
+			int j = 0,z;
 			for (j; j < 64 - i; j++) arr[j] = 1;
 			for (int x = 0; x < i; x++) {
 				arr[j++] = rev[x];
@@ -127,6 +129,7 @@ void BinaryHex (long long int n) {
 			}
 		}
 	}
+	//else given integer unsigned value
 	else {
 		temp = n;
 		int j = 0;
@@ -138,15 +141,12 @@ void BinaryHex (long long int n) {
 		}
 		for (int a = i-1, b = 0; b <= i-1; b++, a--) rev[b] = arr[a];
 		if (i <=7 && i >= 0) {
-			int z;
 			for (j; j < 8 - i; j++) arr[j] = 0;
 			for (int x = 0; x < i; x++)arr[j++] = rev[x];
 		} else if (i <= 15 && i >= 8) {
-			int z;
 			for (j; j < 16 - i; j++) arr[j] = 0;
 			for (int x = 0; x < i; x++) arr[j++] = rev[x];
 		} else if (i <= 31 && i >= 16) {
-			int z;
 			for (j; j < 32 - i; j++) arr[j] = 0;
 			for (int x = 0; x < i; x++) arr[j++] = rev[x];
 		}
@@ -168,23 +168,24 @@ void BinaryHex (long long int n) {
 	}
 	Hex (temp, ptr);
 }
-
-int power (int m) {
+/// <summary>it's return the 2 power of given value</summary>
+int Power (int m) {
 	int mul=1;
 	for (int i = 0; i < m; i++) mul *= 2;
 	return mul;
 }
 
-void Hex (long int  n,int *ptr) {
+/// <summary> This function called by BinaryAndHexConverter function and get binary values from that function and print hexadecimal value</summary>
+void Hex (long long int  n,int *ptr) {
 	printf ("\n");
 	i = 0;
-	int m = 0;
-	int stepin = 0;
+	int m = 0,stepin = 0;
 	printf ("HEX:");
+	//Binary value converted into hexadecimal
 	for (int k = 0; k < (h + 1) / 4; k++) {
 		int sum = 0;
 		for (int j = 0, x = 3; j < 4; x--, j++) {
-			sum += (*ptr) * power (x);
+			sum += (*ptr) * Power (x);
 			ptr++;
 		}
 		if ((sum == 0) && (stepin == 0)) continue;
