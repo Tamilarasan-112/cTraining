473bb8799eeb9f4a2551a9979292a18bf8ded762
Tamilarasan-112 <Tamilarasan.Ramalingam@trumpf.com>
2024-09-26 16:16:58 +0530

    A4:Feedbacks are resolved

--- cTraining/cTraining/cTraining.c
+++ cTraining/cTraining/cTraining.c
@@ -28,8 +28,8 @@
 /// <summary>Return true if input is valid.</summary>
 bool IsValidChoice (char input[]);
 
-/// <summary>Get the user input and return return successful or not.</summary>
-int GetText ();
+/// <summary>Get the user input.</summary>
+void GetText ();
 
 /// <summary>Return true if input text is valid.</summary>
 bool IsValidText (char sent[]);
@@ -40,8 +40,8 @@ int Display (char Text[]);
 /// <summary>Return true if the input text is palindrome.</summary>
 bool IsPalindromeText (char arr[]);
 
-/// <summary>Get the number from user and return successful or not. </summary>
-int GetInt ();
+/// <summary>Get the number from user. </summary>
+void GetInt ();
 
 /// <summary>Return reversed version of given number.</summary>
 int ReverseNum (int num);
@@ -78,17 +78,20 @@ bool IsValidChoice (char input[]) {
    return true;
 }
 
-int GetText () {
+void GetText () {
    char sent[110];
-   printf ("\nInput:");
-   fgets (sent, 110, stdin);
-   if (IsValidText (sent)) return Display (sent);
-   else {
-      printf (RED_COLOR"\nInvalid!\n"RESET_COLOR);
-      if (sent[strlen (sent) - 1] != '\n')ClearInputBuffer ();
-      return GetText ();
-   }
-   return UNSUCCESSFUL;
+   bool isExit = false;
+   do {
+      printf ("\nInput:");
+      fgets (sent, 110, stdin);
+      if (IsValidText (sent)) {
+         isExit = true;
+         Display (sent);
+      } else {
+         printf (RED_COLOR"\nInvalid!\n"RESET_COLOR);
+         if (sent[strlen (sent) - 1] != '\n')ClearInputBuffer ();
+      }
+   } while (!isExit);
 }
 
 bool IsValidText (char input[]) {
@@ -123,17 +126,20 @@ int Display (char sent[]) {
    return UNSUCCESSFUL;
 }
 
-int GetInt () {
+void GetInt () {
    char input[15];
-   printf ("\ninput:");
-   fgets (input, 15, stdin);
-   if (IsValidInt (input)) return DisplayInt (atoi (input));
-   else {
-      printf (RED_COLOR"Invalid!\n"RESET_COLOR);
-      if (input[strlen (input) - 1] != '\n') ClearInputBuffer ();
-      return GetInt ();
-   }
-   return UNSUCCESSFUL;
+   bool isExit = false;
+   do {
+      printf ("\ninput:");
+      fgets (input, 15, stdin);
+      if (IsValidInt (input)) {
+         isExit = true;
+         DisplayInt (atoi (input));
+      } else {
+         printf (RED_COLOR"Invalid!\n"RESET_COLOR);
+         if (input[strlen (input) - 1] != '\n') ClearInputBuffer ();
+      }
+   } while (!isExit);
 }
 
 bool IsValidInt (char input[]) {
@@ -141,9 +147,14 @@ bool IsValidInt (char input[]) {
    char** endPtr = NULL;
    int strLength = strlen (input);
    if (strLength - 1 > 10 || strtoll (str, endPtr, 10) > 2147483647)return false;
-   for (int j = 0, k = 1; j < strLength - 1; j++, k++) {
-      if (input[0] == '-' && input[1] != '\n'&& isdigit(input[k]))continue;
-      if (!(isdigit (input[j])))return false;
+   if (input[0] == '-' && input[1] != '\n') {
+      for (int k = 1; k < strLength - 2; k++) {
+         if (!isdigit (input[k]))return false;
+      }
+   } else {
+      for (int j = 0, k = 1; j < strLength - 1; j++, k++) {
+         if (!(isdigit (input[j])))return false;
+      }
    }
    return true;
 }
@@ -180,26 +191,28 @@ void ClearInputBuffer () {
 }
 
 void Test () {
-   printf ("__________________________________\nMain Menu:\nAutomation testing  ->  Enter 1.\nManual testing");
-   printf ("      ->  Enter 2.\nExit                ->  Enter 3.\n__________________________________\n");
-   printf (MEGENT_COLOR"Choice:"RESET_COLOR);
    char choice[4];
-   fgets (choice, 4, stdin);
-   if (IsValidChoice (choice)) {
-      switch (choice[0]) {
-      case '1':
-         AutomationTest ();
-         break;
-      case '2':
-         ManualTest ();
-         break;
-      case '3':break;
+   bool isExit = false;
+   do {
+      printf ("__________________________________\nMain Menu:\nAutomation testing  ->  Enter 1.\nManual testing");
+      printf ("      ->  Enter 2.\nExit                ->  Enter 3.\n__________________________________\n");
+      printf (MEGENT_COLOR"Choice:"RESET_COLOR);
+      fgets (choice, 4, stdin);
+      if (IsValidChoice (choice)) {
+         switch (choice[0]) {
+         case '1':
+            AutomationTest ();
+            break;
+         case '2':
+            ManualTest ();
+            break;
+         case '3':isExit=true;
+         }
+      } else {
+         if (choice[strlen (choice) - 1] != '\n') ClearInputBuffer ();
+         printf (RED_COLOR"Invalid!\n"RESET_COLOR);
       }
-   } else {
-      if (choice[strlen (choice) - 1] != '\n') ClearInputBuffer ();
-      printf (RED_COLOR"Invalid!\n"RESET_COLOR);
-      Test ();
-   }
+   } while (!isExit);
 }
 
 void ManualTest () {
@@ -218,22 +231,18 @@ void ManualTest () {
          case '2':
             GetInt ();
             break;
-         case '3':
-            Test ();
-            isExit = true;
-            break;
+         case '3':isExit = true;
          }
       } else {
          if (choice[strlen (choice) - 1] != '\n') ClearInputBuffer ();
          printf (RED_COLOR"Invalid!\n"RESET_COLOR);
-         ManualTest ();
       }
    } while (!isExit);
 }
 
 void AutomationTest () {
-   char textTest[][235] = { "Don’t nod","Was it a car or a cat I saw?","Madam!","Malayalam","Civic","Kukku","Bib","Nun","Level","I did, did I ? ","Mom","Dada",
-   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n","Appa" };
+   char textTest[][200] = { "Don’t nod","Was it a car or a cat I saw?","Madam!","Malayalam","Civic","Kukku","Bib","Nun","Level","I did, did I ? ","Mom","Dada",
+ "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad\n","Appa" };
    char numTest[][20] = { "121","94867","123","34543","1","1212132","00000","12101","919","18881","- 112","2147483648","3abs","Amma" };
    for (int i = 0, n = 1; i < 14; i++, n++) {
       printf ("\ncase%d :\n", n);
