d1a53d80250140caa5e22392f9a9854aab237266
Tamilarasan-112 <Tamilarasan.Ramalingam@trumpf.com>
2024-11-22 12:55:23 +0530

    calculate number of coins

--- cTraining/cTraining/cTraining.c
+++ cTraining/cTraining/cTraining.c
@@ -8,8 +8,39 @@
 
 #include <stdio.h>;
 
-int main () {
 
-	printf ("hello,Naveen");
+
+
+
+int main () {
+	int paid;
+	int Amound;
+	int coins=0;
+	char c;
+	scanf_s("%d", &paid);
+	scanf_s("%d", &Amound);
+	int change = paid - Amound;
+	CalcCoins (&coins, change);
+	printf ("%d", coins);
+}
+
+int CalcCoins (int *coins, int change) {
+	int coin[4] = { 10,5,2,1 };
+	int i = 0;
+	while (change > 0) {
+		if (change % 10 == 0) {
+			(*coins)++;
+			change /= 10;
+		} else if (change % 5 == 0) {
+			(*coins)++;
+			change /= 5;
+		} else if (change % 2 == 0) {
+			(*coins)++;
+			change /= 2;
+		} else if (change % 1 == 0) {
+			(*coins)++;
+			change /= 1;
+		}
+	}
 
 }
