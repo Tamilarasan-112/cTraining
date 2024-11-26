1cd1202dd3d4e67d3a63bd11574171d9f011def7
Tamilarasan-112 <Tamilarasan.Ramalingam@trumpf.com>
2024-11-26 19:26:03 +0530

    corrected

--- cTraining/cTraining/cTraining.c
+++ cTraining/cTraining/cTraining.c
@@ -9,77 +9,132 @@
 #include <stdio.h>;
 #include <math.h>
 #include <malloc.h>
+#include <stdbool.h>
+#pragma warning(disable : 4996)
 
 struct ComplexNumber {
-	char real[15];
-	char operator;
-	char imagin[15];
+	float real ;
+	float imagin;
 };
 
 typedef struct ComplexNumber CompNum;
 
-char* Calc (CompNum* num1,CompNum* num2, char operation) {
-	num1->imagin[0] = '0';
-	num2->imagin[0] = '0';
-	char* value = "a+ib";
+void Calc (CompNum* num1,CompNum* num2, char operation,CompNum* value) {
 	switch (operation) {
 	case '+':
-		value[0] = (atoi (num1->real) + atoi (num2->real))+49;
-		value[3] = (atoi (num1->imagin) + atoi (num2->imagin))+49;
-		return value;
+		value->real = num1->real + num2->real;
+	   value->imagin=num1->imagin + num2->imagin ;
+		break;
 	case '-':
-		value[0] = (atoi (num1->real) > atoi (num2->real)) ? (atoi (num1->real) - atoi (num2->real))+49 : (atoi (num2->real) - atoi (num1->real)) + 49;
-		value[3] = (atoi (num1->imagin) > atoi (num2->imagin)) ? (atoi (num1->imagin) - atoi (num2->imagin)) + 49 : (atoi (num2->imagin) - atoi (num1->imagin)) + 49;
-		return value;
+		value->real = num1->real - num2->real;
+		value->imagin=num1->imagin - num2->imagin;
+		break;
 	case '*':
-		value[0] = ((atoi(num1->real) * atoi(num2->real)) - (atoi(num1->imagin) *atoi(num2->imagin)))+49;
-		value[3] = ((atoi (num1->real) * atoi (num2->real)) + (atoi (num1->imagin) * atoi (num2->imagin))) + 49;
-		return value;
+		value->real = num1->real * num2->real - num1->imagin * num2->imagin;
+		value->imagin=num1->real * num2->real + num1->imagin * num2->imagin;
 	}
 }
 
-char* Add (CompNum* num1, CompNum* num2) {
+void Add (CompNum* num1, CompNum* num2,CompNum* value) {
 	char operation = '+';
-	return Calc (num1, num2, operation);
+	Calc (num1, num2, operation,value);
 }
 
-char* Sub (CompNum* num1, CompNum* num2) {
+void Sub (CompNum* num1, CompNum* num2, CompNum* value) {
 	char operation = '-';
-	return Calc (num1, num2, operation);
+	Calc (num1, num2, operation, value);
 }
 
-char* Multi(CompNum* num1, CompNum* num2) {
+void Multi(CompNum* num1, CompNum* num2, CompNum* value) {
 	char operation = '*';
-	return Calc (num1, num2, operation);
+	Calc (num1, num2, operation,value);
 }
 
-char* Mod (CompNum *num) {
-	char* value = "a+ib";
-	*value = (pow (atoi (num->real) * atoi (num->real) + atoi (num->imagin) * atoi (num->imagin),0.5))+49;
-	return value;
+void Mod (CompNum *num,CompNum* value) {
+	value->imagin=pow (num->real* num->real + num->imagin * num->imagin,0.5);
 }
 
-char* conjucate (CompNum* num) {
-	char* value = "a+ib";
-	value[0] = atoi (num->real)+49;
-	value[1]=(num->operator == '-') ? '+' : '-';
-	value[3] = atoi (num->imagin) + 49;
-	return value;
+void conjucate (CompNum* num,CompNum* value) {
+	value->imagin = -(num->imagin);
+	value->real = num->real;
+}
+
+void ClearInputBuffer () {
+	char c;
+	while (c = getchar () != '\n');
+}
+
+int GetInput (CompNum *num) {
+	bool isValid1, isValid2;
+	char c, c1;
+	do {
+		isValid1 = true;
+		isValid2 = true;
+		printf ("Enter real number: ");
+		if (!scanf_s ("%f%c", &num->real, &c1, 1)) {
+			ClearInputBuffer ();
+			isValid1 = false;
+		} else if (c1 != '\n') {
+			ClearInputBuffer ();
+			isValid1 = false;
+		}
+		printf ("Enter imaginary number: ");
+		if (!scanf_s ("%f%c", &num->imagin, &c, 1)) {
+			ClearInputBuffer ();
+			isValid2 = false;
+		} else if (c != '\n') {
+			ClearInputBuffer ();
+			isValid2 = false;
+		}
+		if (!(isValid1 && isValid2)) printf ("Invalid complex number!\n");
+	} while (!(isValid1 && isValid2));
+}
+
+bool isValidChoice (char* choice) {
+	return choice[strlen (choice) - 1] == '\n' && (choice[0] == '0' || choice[0] == '1');
 }
 
 int main () {
-	CompNum* num1 = malloc (sizeof (CompNum));
-	CompNum* num2 = malloc (sizeof (CompNum));
-	if (num1 != NULL) {
-		num1->imagin[15] = "2131";
-		num1->operator='+';
-		num1->real[15] = "32";
-	}
-	if (num2 != NULL) {
-		num1->imagin[15] = "2131";
-		num1->operator='+';
-		num1->real[15] = "32";
-	}
-
-	printf("%s",Add (num1, num2));
+	bool isExit = false;
+	do {
+		char choice[3];
+		printf ("Enter 0 if you want to exit, else enter 1. \nchoice:");
+		fgets (choice, 3, stdin);
+		if (isValidChoice (choice)) {
+			if (choice[0] == '0') {
+				isExit = true;
+			}
+			else {
+				CompNum* num1 = malloc (sizeof (CompNum));
+				CompNum* num2 = malloc (sizeof (CompNum));
+				CompNum* value = malloc (sizeof (CompNum));
+				if (num1 != NULL && num2 != NULL) {
+					printf ("Complex number 1:\n");
+					GetInput (num1);
+					printf ("Complex number 2:\n");
+					GetInput (num2);
+				}
+				if (value != NULL) {
+					Add (num1, num2, value);
+					printf ("Addtion       :%f %fi\n", value->real, value->imagin);
+					Sub (num1, num2, value);
+					printf ("Subtraction   :%f %fi\n", value->real, value->imagin);
+					Multi (num1, num2, value);
+					printf ("Multiplication:%f %fi\n", value->real, value->imagin);
+					Mod (num1, value);
+					printf ("Modulus1       :%f\n", value->imagin);
+					Mod (num2, value);
+					printf ("Modulus2       :%f\n", value->imagin);
+					conjucate (num1, value);
+					printf ("Conjugate1     :%f %fi\n", value->real, value->imagin);
+					conjucate (num2, value);
+					printf ("Conjugate2     :%f %fi\n", value->real, value->imagin);
+				}
+			}
+		}
+		else {
+			printf ("Invalid choice!\n");
+			if (choice[strlen (choice) - 1] != '\n')ClearInputBuffer ();
+		}
+	} while (!isExit);
 }
