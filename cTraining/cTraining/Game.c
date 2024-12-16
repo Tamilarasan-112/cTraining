0534d9c77463cee6a7e4aa8bc3fb6d61f845ca37
Tamilarasan-112 <Tamilarasan.Ramalingam@trumpf.com>
2024-12-16 17:24:46 +0530

    Feedbacks are resolved

--- cTraining/cTraining/Game.c
+++ cTraining/cTraining/Game.c
@@ -1,4 +1,4 @@
-// ------------------------------------------------------------------------------------------------
+﻿// ------------------------------------------------------------------------------------------------
 // Training ~ A training program for new joiners at Metamation, Batch - July 2024.
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
@@ -8,31 +8,25 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-#include <malloc.h>
 #include <cType.h>
-#include <string.h>
+#include <conio.h>
 
-struct point {
-   int player1;
-   int player2;
-};
-
-typedef struct point points;
+#pragma warning(disable : 4996)
 
 /// <summary>Display the board.</summary>
-void Board (char arr[][5]);
+void InitBoard (char board[][4]);
 
 /// <summary>Get the input from the player.</summary>
 void Input (int* player);
 
-/// <summary>Return true if input is valid,else false.</summary>
-bool IsValidInput (char* playerInput);
-
 /// <summary>Marks the player's symbol on the board.</summary>
-void MarkSymbols (int player, char arr[][5], char* sym, int index, int* counts);
+/// <param name="player">Player input</param>
+/// <param name="board">Contains Board data</param>
+/// <param name="sym">Player symbol</param>
+int MarkSymbols (int player, char board[][4], char sym);
 
 /// <summary>To manually test all the methods.</summary>
-void ManualTest ();
+void StartGame ();
 
 /// <summary>To automatically test all the methods.</summary>
 void AutomationTest ();
@@ -41,119 +35,77 @@ void AutomationTest ();
 int TestMarkSymbols (int input[], char expOut[][5]);
 
 /// <summary>To test the Check method.</summary>
-void TestCheck (char MarkedBoard[][5], int exp);
-
-/// <summary>Displays the winner or indicates a draw match.</summary>
-void DisplayWinner (int out);
-
-/// <summary>To test the IsValidInput method.</summary>
-void TestIsValidInput ();
+void TestCheckWin (char markedBoard[][4], int exp);
 
 int main () {
-  //ManualTest ();
-   AutomationTest ();
+   StartGame ();
+   //AutomationTest ();
 }
 
-void MarkSymbols (int player, char arr[][5], char* sym, int index, int* counts) {
-   if (player <= 3) {
-      if (isdigit (arr[0][player - 1])) {
-         arr[0][player - 1] = sym[index];
-         (*counts)++;
-      }
-   } else if (player <= 6) {
-      if (isdigit (arr[1][player - 4])) {
-         arr[1][player - 4] = sym[index];
-         (*counts)++;
-      }
-   } else {
-      if (isdigit (arr[2][player - 7])) {
-         arr[2][player - 7] = sym[index];
-         (*counts)++;
-      }
+int MarkSymbols (int player, char board[][4], char sym) {
+   int row = player <= 3 ? 0 : player <= 6 ? 1 : 2;
+   int col = player <= 3 ? player - 1 : player <= 6 ? player - 4 : player - 7;
+   if (isdigit (board[row][col])) {
+      board[row][col] = sym;
+      return 1;
    }
+   return 0;
 }
 
-int Check (char arr[][5], char* sym) {
-   points row[3], column[3], diagonal1, diagonal2;
-   diagonal1.player1 = 0;
-   diagonal1.player2 = 0;
-   diagonal2.player1 = 0;
-   diagonal2.player2 = 0;
-   //for intiallization
+int CheckWin (char board[][4], char sym) {
    for (int i = 0; i < 3; i++) {
-      column[i].player1 = 0;
-      column[i].player2 = 0;
+      //checking columns and rows
+      if ((board[i][0] == sym && board[i][1] == sym && board[i][2] == sym) ||
+         (board[0][i] == sym && board[1][i] == sym && board[2][i] == sym))
+         return 1;
    }
-   for (int i = 0, k = 2; i < 3; i++, k--) {
-      row[i].player1 = 0;
-      row[i].player2 = 0;
-      for (int j = 0; j < 3; j++) {
-         if (arr[i][j] == sym[0]) {
-            row[i].player1++;
-            column[j].player1++;
-            if (i == j) diagonal1.player1++;
-            if (j == k) diagonal2.player1++;
-         } else if (arr[i][j] == sym[1]) {
-            row[i].player2++;
-            column[j].player2++;
-            if (i == j) diagonal1.player2++;
-            if (j == k) diagonal2.player2++;
-         }
-         if (row[i].player1 == 3 || column[j].player1 == 3 || diagonal1.player1 == 3 || diagonal2.player1 == 3) {
-            return 0;
-         } else if (row[i].player2 == 3 || column[j].player2 == 3 || diagonal1.player2 == 3 || diagonal2.player2 == 3) {
-            return 1;
-         }
-      }
-   }
-   return -1;
+   //checking diagonals
+   if ((board[0][2] == sym && board[1][1] == sym && board[2][0] == sym) ||
+      (board[0][0] == sym && board[1][1] == sym && board[2][2] == sym))
+      return 1;
+   return 0;
 }
 
-void Board (char arr[][5]) {
+void InitBoard (char board[][4]) {
+   printf ("\n");
    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 4; j++) {
-         printf ("|%c", arr[i][j]);
-      }
+      for (int j = 0; j < 4; j++)
+         printf ("|%c", board[i][j]);
    }
 }
 
 void Input (int* player) {
-   char* playerInput = malloc (4 * sizeof (char));
-   if (playerInput != NULL) {
-      do {
-         fgets (playerInput, 4, stdin);
-      } while (!(IsValidInput (playerInput)));
-      *player = playerInput[0] - '0';
-   }
-}
-
-bool IsValidInput (char* playerInput) {
-   return strlen (playerInput) == 2 && (playerInput[0] >= '1' && playerInput[0] <= '9');
+   char playerInput;
+   do {
+      playerInput = getch ();
+   } while (!(playerInput >= '1' && playerInput <= '9'));
+   *player = playerInput - '0';
 }
 
 void StartGame () {
-   char arr[10][5] = { { '1','2','3','\n' },{ '4','5','6','\n' },{ '7','8','9','\n' } };
-   int player, it = 0, ret, i = 0, j = 0, counts = 0;
-   bool isWin = false;
-   char sym[3] = { '+','*' };
-   Board (arr);
-   while (!isWin) {
+   char board[][4] = { { '1','2','3','\n' },{ '4','5','6','\n' },{ '7','8','9','\n' } };
+   int player, it = 0, ret, i = 0, j = 0;
+   bool isGameOver = false;
+   char sym;
+   InitBoard (board);
+   while (!isGameOver) {
+      sym = i ? 'O' : 'X';
       it++;
-      printf ("Player%d:", i + 1);
-      Input (&player);
-      MarkSymbols (player, arr, sym, i, &counts);
-      Board (arr);
+      do {
+         printf ("\nPlayer%d:", i + 1);
+         Input (&player);
+         ret = MarkSymbols (player, board, sym);
+         if (!ret)printf ("Position is occupied!\n");
+      } while (!ret);
+      InitBoard (board);
       if (it >= 5) {
-         ret = Check (arr, sym);
-         if (!ret) {
-            printf ("\nPlayer1 is win\n");
-            isWin = true;
-         } else if (ret) {
-            printf ("\nPlayer2 is win\n");
-            isWin = true;
-         } else if (counts == 9) {
-            printf ("\nDraw");
-            isWin = true;
+         ret = CheckWin (board, sym);
+         if (ret) {
+            printf ("\nPlayer%d is win\n", i + 1);
+            isGameOver = true;
+         } else if (it == 9) {
+            printf ("\nMatch draw!");
+            isGameOver = true;
          }
       }
       i++;
@@ -161,85 +113,75 @@ void StartGame () {
    }
 }
 
-void ManualTest () {
-   StartGame ();
-}
-
 void AutomationTest () {
    int input1[9] = { 1,2,3,4,5,6,7,8,9 }, input2[9] = { 2,3,1,4,6,8,7,5,9 };
-   char expout1[10][5] = { { '+','*','+','\n' },{ '*','+','*','\n' },{ '+','*','+','\n' } };
-   char expout2[10][5] = { { '+','+','*','\n' },{ '*','*','+','\n' },{ '+','*','+','\n' } };
+   int input3[9] = { 1,2,2,1,3,4,3,5,6 };
+   char expOut1[][4] = { { 'O','X','O','\n' },{ 'X','O','X','\n' },{ 'O','X','O','\n' } };
+   char expOut2[][4] = { { 'O','O','X','\n' },{ 'X','X','O','\n' },{ 'O','X','O','\n' } };
+   char expOut3[][4] = { {'O','X','O','\n' },{ 'X','X','O','\n' },{ '7','8','9','\n' } };
 
    printf ("\nTest MarkSymbols Method:\n");
    printf ("\nCase 1:\n");
-   TestMarkSymbols (input1, expout1);
+   TestMarkSymbols (input1, expOut1);
    printf ("\nCase 2:\n");
-   TestMarkSymbols (input2, expout2);
+   TestMarkSymbols (input2, expOut2);
+   printf ("\nCase 3:\n");
+   TestMarkSymbols (input3, expOut3);
 
    printf ("\nTest Check Method:\n");
    printf ("\nCase 1:\n");
-   TestCheck (expout1, 0);
+   TestCheckWin (expOut1, 1);
    printf ("\nCase 2:\n");
-   TestCheck (expout2, -1);
-
-   printf ("\nTest input validate Method:\n ");
-   TestIsValidInput ();
+   TestCheckWin (expOut2, 0);
+   printf ("\nCase 2:\n");
+   TestCheckWin (expOut3, 0);
 }
 
 int TestMarkSymbols (int input[], char expOut[][5]) {
-   char arr[10][5] = { { '1','2','3','\n' },{ '4','5','6','\n' },{ '7','8','9','\n' } };
-   char sym[3] = { '+','*' };
-   int j = 0, i = 0, counts = 0;
+   char board[][4] = { { '1','2','3','\n' },{ '4','5','6','\n' },{ '7','8','9','\n' } };
+   char sym;
+   int j = 0, i = 0;
    while (i < 9) {
-      printf ("player%d:%d\n", j + 1, input[i]);
-      MarkSymbols (input[i], arr, sym, j, &counts);
+      sym = i % 2 == 0 ? 'O' : 'X';
+      printf ("Player%d:%d\n", j + 1, input[i]);
+      MarkSymbols (input[i], board, sym);
       j++;
       i++;
       if (j > 1) j = 0;
    }
    printf ("\nActual Output:\n");
-   Board (arr);
+   InitBoard (board);
    printf ("\nExpected OutPut:\n");
-   Board (expOut);
+   InitBoard (expOut);
    for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; j++) {
-         if (!(arr[i][j] == expOut[i][j])) {
+         if (!(board[i][j] == expOut[i][j])) {
             printf ("Failed!\n");
             return 0;
          }
       }
    }
-   printf ("passed!\n");
+   printf ("Passed!\n");
    return 1;
 }
 
-void TestCheck (char MarkedBoard[][5], int exp) {
+void TestCheckWin (char markedBoard[][4], int exp) {
    int ret;
-   char sym[3] = { '+','*' };
+   char sym;
    printf ("\nInput:\n");
-   Board (MarkedBoard);
-   ret = Check (MarkedBoard, sym);
-   printf ("\nExpected output:\n");
-   DisplayWinner (exp);
-   printf ("\nActual output:\n");
-   DisplayWinner (ret);
-   if (ret == exp) printf ("\nPassed!\n");
-   else printf ("\nFailed!\n");
-}
-
-void TestIsValidInput () {
-   char input[][5] = { "1\n","2\n","23\n","10\n","0\n","\n","s\n","-1\n","7\n" };
-   int expout[] = { 1,1,0,0,0,0,0,0,1 }, ret;
-   for (int i = 0; i < 9; i++) {
-      ret = IsValidInput (input[i]);
-      printf ("\ncase%d:\ninput:%sexpected:%d\nactual:%d", i + 1, input[i], expout[i], ret);
-      if (ret == expout[i]) printf ("\nPassed!\n");
-      else printf ("\nFailed!\n");
+   InitBoard (markedBoard);
+   for (int i = 0; i < 2; i++) {
+      sym = i % 2 == 0 ? 'O' : 'X';
+      ret = CheckWin (markedBoard, sym);
+      printf ("\nExpected output:\n");
+      exp ? printf ("Player%d is win", i + 1) : printf ("Match draw!");
+      printf ("\nActual output:\n");
+      if (ret) {
+         printf ("Player%d is win", i + 1);
+         break;
+      } else {
+         i ? printf ("Match draw!") : printf ("Wait...");
+      }
    }
-}
-
-void DisplayWinner (int out) {
-   if (!out) printf ("player1 is win\n");
-   else if (out) printf ("player2 is win\n");
-   else printf ("match draw\n");
+   printf (ret == exp ? "\nPassed!\n" : "\nFailed!\n");
 }
